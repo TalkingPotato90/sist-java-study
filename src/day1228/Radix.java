dd2b66c3d71de25b3d6cad50e4a9aa43a2456f62
potatomoong <star901210@hanmail.net>
2023-12-28 17:50:15 +0900

    Feat: 23.12.28 수업 내용 예제

---
+++ src/day1228/Radix.java
@@ -0,0 +1,48 @@
+package day1228;
+
+/*
+진수 사용
+*/
+class Radix {
+    public static void main(String[] args) {
+
+        int i = 10; // 10진수 -> 2진수로 저장
+        int j = 012; // 8진수 -> 2진수로 저장
+        int k = 0xA; // 16진수 -> 2진수로 저장
+
+        System.out.println(i + j + k); // 2진수를 연산하여 10진수로 보여준다
+
+        i = 25;
+        System.out.println(i + "를 2진수로 : " + Integer.toBinaryString(i));
+
+        i = 89;
+        System.out.println(i + "를 2진수로 : " + Integer.toBinaryString(i));
+
+        i = 300;
+        System.out.println(i + "를 2진수로 : " + Integer.toBinaryString(i));
+
+        System.out.println("==============================================");
+
+        System.out.println("0X25를 2진수로 : " +
+                Integer.toBinaryString(0X25));
+        System.out.println("0X1D를 2진수로 : " +
+                Integer.toBinaryString(0X1D));
+        System.out.println("0XFF를 2진수로 : " +
+                Integer.toBinaryString(0XFF));
+
+        System.out.println("==============================================");
+
+        System.out.println("10을 8진수로 : " +
+                Integer.toOctalString(10));
+        System.out.println("200을 8진수로 : " +
+                Integer.toOctalString(10));
+
+        System.out.println("==============================================");
+
+        System.out.println("25 를 16진수로 : "
+                + Integer.toHexString(25));
+        System.out.println("300 를 16진수로 : "
+                + Integer.toHexString(300));
+
+    } // main
+} // class
