ac2adcd8c94f6610c28cea03dd655cbd041a8060
potatomoong <star901210@hanmail.net>
2024-01-09 17:51:52 +0900

    Feat: 24.01.09 수업 내용 예제

--- src/main/day0108/Work0108.java
+++ src/main/day0108/Work0108.java
@@ -2,49 +2,54 @@ package day0108;
 
 public class Work0108 {
 
-    // 1. 자신의 이름을 출력하는 method
-    // 2. 자신의 이름 중에 성을 반환하는 method
-    // 3. 점수를 입력받아 점수의 판정을 출력하는 method 작성
-    // 점수는  0 ~ 100사이 일때만 판정을 하고 그렇지 않으면 "잘못된 점수"를 출력
-    // 0~ 39 : 과락 출력, 40 ~59 : 다른 과목을 참조, 60~100 : 합격
-    // 4. 문자를 입력방아 해당 문자가 "대문자", "소문자", "숫자" 인지를 판단하여 출력하는 method.
-    // A - 대문자
-    // 그 외에는 $ - 대문자, 소문자, 숫자가 아닙니다. 를 출력
-    // 5. 태어난 해를 입력받아 나이를 반환하는 method
-    // 6. 3개의 수를 입력받아 가장 "작은 수"를 반환하는 일을 하는 method
-    public void fullName(){
+    public void printFullName(){
         System.out.println("홍길동");
     }
 
-    public void lastName(){
-        System.out.println("홍");
+    public char printLastName(){
+        return '홍';
     }
 
-    public String checkGrade(int score){
-        String grade = null;
-        if (score >= 0 && score <= 39){
-            grade = "과락";
+//    public String checkGrade(int score){
+//        if (score < 0 || score > 100) {
+//            return "잘못된 점수";
+//        } else if (score >= 0 && score <= 39){
+//            return "과락";
+//        } else if(score >= 40 && score <= 59){
+//            return  "다른 과목 참조";
+//        }
+//
+//        return "합격";
+//    }
+
+    public void checkGrade(int score){
+        if (score < 0 || score > 100) {
+            System.out.println("잘못된 점수");
+        } else if (score >= 0 && score <= 39){
+            System.out.println("과락");
         } else if(score >= 40 && score <= 59){
-            grade = "다른 과목 참조";
-        } else if(score >= 60 && score <= 100){
-            grade = "합격";
-        }
-
-        if (score >= 0 && score <= 100){
-         return grade;
-        }
-        return "잘못된 점수";
+            System.out.println("다른 과목 참조");
+        }else System.out.println("합격");
     }
 
-    public String checkCharacterType(char character){
+//    public String checkCharacterType(char character){
+//        if(character>= 'A' && character <= 'Z'){
+//            return "대문자";
+//        } else if(character>='a' && character <= 'z'){
+//            return "소문자";
+//        } else if(character>= '0' && character<= '9') {
+//            return "숫자";
+//        }
+//        return "대문자, 소문자, 숫자가 아닙니다.";
+//    }
+    public void checkCharacterType(char character){
         if(character>= 'A' && character <= 'Z'){
-            return "대문자";
+            System.out.println("대문자");
         } else if(character>='a' && character <= 'z'){
-            return "소문자";
+            System.out.println("소문자");
         } else if(character>= '0' && character<= '9') {
-            return "숫자";
-        }
-        return "대문자, 소문자, 숫자가 아닙니다.";
+            System.out.println("숫자");
+        } else System.out.println("대문자, 소문자, 숫자가 아닙니다.");
     }
 
     public int calculateAge(int birthYear){
@@ -53,6 +58,7 @@ public class Work0108 {
     }
 
     public int checkSmallestNumber(int num1, int num2, int num3){
+//    return Math.min(num1,(Math.min(num2,num3)));
         if (num1 < num2 && num1 < num3){
             return num1;
         } else if(num2 < num1 && num2 < num3){
@@ -64,18 +70,28 @@ public class Work0108 {
     public static void main(String[] args) {
         Work0108 work0108 = new Work0108();
 
-        work0108.fullName();
-        work0108.lastName();
+    // 1. 자신의 이름을 출력하는 method
+        work0108.printFullName();
 
-        String grade = work0108.checkGrade(100);
-        System.out.println(grade);
+    // 2. 자신의 이름 중에 성을 반환하는 method
+        char lastName = work0108.printLastName();
+        System.out.println(lastName);
 
-        String characterType = work0108.checkCharacterType('s');
-        System.out.println(characterType);
+    // 3. 점수를 입력받아 점수의 판정을 출력하는 method 작성
+//        String grade = work0108.checkGrade(60);
+//        System.out.println(grade);
+        work0108.checkGrade(80);
 
+    // 4. 문자를 입력방아 해당 문자가 "대문자", "소문자", "숫자" 인지를 판단하여 출력하는 method.
+//        String characterType = work0108.checkCharacterType('s');
+//        System.out.println(characterType);
+        work0108.checkCharacterType('A');
+
+    // 5. 태어난 해를 입력받아 나이를 반환하는 method
         int age = work0108.calculateAge(1990);
         System.out.println(age);
 
+    // 6. 3개의 수를 입력받아 가장 "작은 수"를 반환하는 일을 하는 method
         int smallestNumber = work0108.checkSmallestNumber(1,3,0);
         System.out.println(smallestNumber);
     }
---
+++ src/main/day0109/InstantNoodlePrepare.java
@@ -0,0 +1,20 @@
+package day0109;
+
+public class InstantNoodlePrepare {
+    public void preparePowder(){
+
+    }
+
+    public void prepareVegetable(){
+
+    }
+
+    public void prepareNoodle(){
+
+    }
+
+    public void prepareAddEgg(){
+
+    }
+
+}
